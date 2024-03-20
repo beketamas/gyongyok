c1a52ccd1a82381e82d56d34f33eb0ef8a73f57b
beketamas <72067154+beketamas@users.noreply.github.com>
2024-03-20 12:56:59 +0100

    jiji

---
+++ miert/Gyongy.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace miert
+{
+    internal class Gyongy
+    {
+        int x, y, z, e;
+
+        public Gyongy(string sor)
+        {
+            string[] tomb = sor.Split(";");
+            this.x = int.Parse(tomb[0]);
+            this.y = int.Parse(tomb[1]);
+            this.z = int.Parse(tomb[2]);
+            this.e = int.Parse(tomb[3]);
+        }
+
+        public int X => x;
+        public int Y => y;
+        public int Z => z;
+        public int E => e;
+    }
+}
---
+++ miert/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Media.Media3D;
+using System.IO;
+using HelixToolkit.Wpf;
+
+namespace miert
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Gyongy> gyongyok = File.ReadAllLines("gyongyok.txt").Skip(1).Select(x => new Gyongy(x)).ToList();
+        public MainWindow()
+        {
+            InitializeComponent();
+            btnGomb.Click += (s,e) => 
+            {
+                General();
+            };
+
+
+            
+
+        }
+        public void General()
+        {
+            gyongyok.ForEach(x =>
+            {
+                Random rnd = new();
+                EllipsoidVisual3D gyongy3D = new();
+                gyongy3D.RadiusX = rnd.Next(0, 20);
+                gyongy3D.RadiusY = rnd.Next(0, 20);
+                gyongy3D.RadiusZ = rnd.Next(0, 20);
+                gyongy3D.Center = new Point3D(x.X, x.Z, x.Y);
+                gyongy3D.Fill = new SolidColorBrush(Color.FromRgb((byte)rnd.Next(0, 255), (byte)rnd.Next(0, 255), (byte)rnd.Next(0, 255)));
+
+
+
+                ter.Children.Add(gyongy3D);
+            });
+        }
+    }
+}
